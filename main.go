73e200fa3e92e3dba51753a85007a431aa251b85
littlebear0729 <littlebear0729@protonmail.com>
2021-11-06 13:25:31 +0800

    Output a single HTML page
    Format code into functions

--- main.go
+++ main.go
@@ -11,7 +11,14 @@ import (
 	"strings"
 )
 
-func main() {
+type templateCodeData struct {
+	Filename1, Filename2   string
+	Code1, Code2           string
+	BeginLine1, BeginLine2 int
+	EndLine1, EndLine2     int
+}
+
+func runSim() []string {
 	out, err := exec.Command("bash", "-c", "./sim_c++ -nT -w1 *.cpp").Output()
 	if err != nil {
 		log.Fatal(err)
@@ -24,83 +31,79 @@ func main() {
 			diff = append(diff, s)
 		}
 	}
-	// fmt.Printf("%v\n\n", diff[1:])
-	for i, s := range diff[1:] {
-		fmt.Printf("%d: %s\n", i, s)
-		file1 := strings.Split(s, "|")[0]
-		file2 := strings.Split(s, "|")[1]
+	return diff
+}
 
-		filename1 := strings.Split(file1, ":")[0]
-		line1 := strings.Split(file1, ":")[1][6:]
+func getFiles(f string) (string, string, string, string) {
+	file1 := strings.Split(f, "|")[0]
+	file2 := strings.Split(f, "|")[1]
 
-		filename2 := strings.Split(file2, ":")[0]
-		pos := strings.Index(strings.Split(file2, ":")[1], "[")
-		line2 := strings.Split(file2, ":")[1][6:pos]
+	filename1 := strings.Split(file1, ":")[0]
+	line1 := strings.Split(file1, ":")[1][6:]
 
-		beginLine1, _ := strconv.Atoi(strings.Split(line1, "-")[0])
-		endLine1, _ := strconv.Atoi(strings.Split(line1, "-")[1])
-		beginLine2, _ := strconv.Atoi(strings.Split(line2, "-")[0])
-		endLine2, _ := strconv.Atoi(strings.Split(line2, "-")[1])
+	filename2 := strings.Split(file2, ":")[0]
+	pos := strings.Index(strings.Split(file2, ":")[1], "[")
+	line2 := strings.Split(file2, ":")[1][6:pos]
+
+	return filename1, line1, filename2, line2
+}
+
+func getLines(s string) (int, int) {
+	beginLine, _ := strconv.Atoi(strings.Split(s, "-")[0])
+	endLine, _ := strconv.Atoi(strings.Split(s, "-")[1])
+
+	return beginLine, endLine
+}
+
+func getCodes(filename string) (int, string) {
+	f, _ := os.Open(filename)
+	var code string
+	scanner := bufio.NewScanner(f)
+	linenum := 0
+	fmt.Println(filename)
+	for scanner.Scan() {
+		code += fmt.Sprintf("%s\n", scanner.Text())
+		linenum++
+	}
+	fmt.Println(code)
+	return linenum, code
+}
+
+func genHtml(filename1 string, code1 string, beginLine1 int, endLine1 int,
+	filename2 string, code2 string, beginLine2 int, endLine2 int) {
+
+	templateCode, _ := template.ParseFiles("layout_code.html")
+	data := templateCodeData{
+		Filename1:  filename1,
+		Filename2:  filename2,
+		Code1:      code1,
+		Code2:      code2,
+		BeginLine1: beginLine1 - 1,
+		BeginLine2: beginLine2 - 1,
+		EndLine1:   endLine1,
+		EndLine2:   endLine2,
+	}
+	// fmt.Println(data)
+	f, _ := os.Create(filename1 + "-" + filename2 + ".html")
+	templateCode.Execute(f, data)
+	f.Close()
+}
+
+func main() {
+	diff := runSim()
+
+	for _, s := range diff[1:] {
+		fmt.Println(s)
+		filename1, line1, filename2, line2 := getFiles(s)
+		beginLine1, endLine1 := getLines(line1)
+		beginLine2, endLine2 := getLines(line2)
 
 		fmt.Printf("filename1: %s, from: %d, to: %d\n", filename1, beginLine1, endLine1)
 		fmt.Printf("filename2: %s, from: %d, to: %d\n", filename2, beginLine2, endLine2)
-		fmt.Println()
 
-		f1, _ := os.Open(filename1)
-		f2, _ := os.Open(filename2)
+		_, code1 := getCodes(filename1)
+		_, code2 := getCodes(filename2)
 
-		var code1, code2 string
-
-		scanner := bufio.NewScanner(f1)
-		linenum := 1
-		fmt.Println(filename1)
-		for scanner.Scan() {
-			code1 += fmt.Sprintf("%s\n", scanner.Text())
-			linenum++
-		}
-		fmt.Println(code1)
-
-		scanner = bufio.NewScanner(f2)
-		linenum = 1
-		fmt.Println(filename2)
-		for scanner.Scan() {
-			code2 += fmt.Sprintf("%s\n", scanner.Text())
-			linenum++
-		}
-		fmt.Println(code1)
-		//code1 = html.EscapeString(code1)
-		//code2 = html.EscapeString(code2)
-
-		f1.Close()
-		f2.Close()
-
-		type templateCodeData struct {
-			Title    string
-			Code     string
-			FromLine int
-			ToLine   int
-		}
-
-		templateCode, _ := template.ParseFiles("layout_code.html")
-		data := templateCodeData{
-			Title:    filename1,
-			Code:     code1,
-			FromLine: beginLine1 - 1,
-			ToLine:   endLine1,
-		}
-		ff, _ := os.Create(filename1 + ".html")
-		templateCode.Execute(ff, data)
-		ff.Close()
-
-		data = templateCodeData{
-			Title:    filename2,
-			Code:     code2,
-			FromLine: beginLine2 - 1,
-			ToLine:   endLine2,
-		}
-		ff, _ = os.Create(filename2 + ".html")
-		templateCode.Execute(ff, data)
-		ff.Close()
-
+		genHtml(filename1, code1, beginLine1, endLine1, filename2, code2, beginLine2, endLine2)
 	}
 }
